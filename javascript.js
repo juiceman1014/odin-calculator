100accc7cc80aa5e1d509d3cb716d722c46c9402
juiceman1014 <justintran1014@gmail.com>
2023-08-14 20:28:11 -0700

    added a function that will call an operator function. created an object for equation components. Created container items and got started on calc-container css

--- javascript.js
+++ javascript.js
@@ -18,5 +18,29 @@ function divide(num1, num2){
     return quotient;
 }
 
+// let num1 = parseFloat(prompt("What's the first number?"));
+// console.log(num1);
+// let num2 = parseFloat(prompt("What's the second number?"));
+// console.log(num2);
+// let operator = prompt("What's the operator?");
 
+let equation = {
+    num1: num1,
+    num2: num2,
+    operator:  operator,
+}
+
+function operate(num1, num2, operator){
+    if(operator == "add"){
+        return add(num1,num2)
+    }else if (operator == "subtract"){
+        return subtract(num1,num2)
+    }else if (operator == "multiply"){
+        return multiply(num1,num2)
+    }else if (operator == "divide"){
+        return divide(num1,num2)
+    }else{
+        return "ERROR"
+    }
+}
 
